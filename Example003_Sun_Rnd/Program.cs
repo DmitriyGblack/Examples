750d07495aa1b66ec66684c28dfdcbaec129fe32
DmitriyG <79133850099@yandex.ru>
2023-07-11 02:34:54 +0700

    Example 003 Sun Rnd

---
+++ Example003_Sun_Rnd/Program.cs
@@ -0,0 +1,11 @@
+﻿// Сложение двух случайных чисел
+int numberA = new Random (). Next (1, 10); // 1 2 3 4 ... 9
+Console.Write("число первое - ");
+Console.WriteLine(numberA);
+int numberB = new Random (). Next (1, 10); // 1 2 3 4 ... 9
+Console.Write("число второе - ");
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write("сумма        - ");
+Console.WriteLine (result);
+

e8ace91c767d77b8743bb7c590341b3a6493665c
DmitriyG <79133850099@yandex.ru>
2023-07-11 14:02:44 +0700

    Example 006 Max

---
+++ Example006_Max/Program.cs
@@ -0,0 +1,13 @@
+﻿// Максимальное число из введенных чисел
+int A = 1;
+int B = 3;
+int C = -16;
+int D = 8;
+int E = 4;
+int max = A;
+if (A > max) max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+if (D > max) max = D;
+if (E > max) max = E;
+Console.WriteLine (max);

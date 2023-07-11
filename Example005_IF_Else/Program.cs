81a5d59e2a40cf4696426950b86baa332bb4bc54
DmitriyG <79133850099@yandex.ru>
2023-07-11 13:23:53 +0700

    Example 005 IF Else

---
+++ Example005_IF_Else/Program.cs
@@ -0,0 +1,14 @@
+﻿// Проверка условий
+Console.Write("Введите имя - ");
+string username = Console.ReadLine ();
+if (username.ToLower() == "маша")
+{
+                        
+                        Console.WriteLine("Ура,это же Маша! Цветы Маше!");
+}
+else
+{
+
+                        Console.Write("Опс. Ну привет ");
+                        Console.WriteLine (username);
+}
